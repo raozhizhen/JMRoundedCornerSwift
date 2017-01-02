9fcf18cdb553fcf598acca7552780e983b28769b
raozhizhen <raozhizhen@baidu-mgame.com>
2017-01-02 12:51:43 +0800

    update readme

--- JMRoundedCornerSwift/JMRoundedCorner.swift
+++ JMRoundedCornerSwift/JMRoundedCorner.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-let jm_operationQueue = NSOperationQueue()
+let jm_operationQueue = OperationQueue()
 
 public struct JMRadius {
     var topLeftRadius: CGFloat
@@ -17,7 +17,7 @@ public struct JMRadius {
     var bottomRightRadius: CGFloat
 }
 
-public func JMRadiusMake(topLeftRadius: CGFloat, _ topRightRadius: CGFloat, _ bottomLeftRadius: CGFloat, _ bottomRightRadius: CGFloat) -> JMRadius {
+public func JMRadiusMake(_ topLeftRadius: CGFloat, _ topRightRadius: CGFloat, _ bottomLeftRadius: CGFloat, _ bottomRightRadius: CGFloat) -> JMRadius {
     
     return JMRadius(topLeftRadius: topLeftRadius, topRightRadius: topRightRadius,bottomLeftRadius: bottomLeftRadius,bottomRightRadius: bottomRightRadius)
 }
@@ -25,45 +25,45 @@ public func JMRadiusMake(topLeftRadius: CGFloat, _ topRightRadius: CGFloat, _ bo
 public extension UIView {
 
     /**给view设置一个 .ScaleAspectFill 模式的圆角背景图*/
-    func radiusWith(radius: CGFloat, backgroundImage: UIImage?) {
-        radiusWith(radius, borderColor: nil, borderWidth: 0, backgroundColor: nil, backgroundImage: backgroundImage, contentMode: .ScaleAspectFill)
+    func radiusWith(_ radius: CGFloat, backgroundImage: UIImage?) {
+        radiusWith(radius, borderColor: nil, borderWidth: 0, backgroundColor: nil, backgroundImage: backgroundImage, contentMode: .scaleAspectFill)
     }
     
     /**给view设置一个 contentMode 模式的圆角背景图*/
-    func radiusWith(radius: CGFloat, backgroundImage: UIImage?, contentMode: UIViewContentMode) {
+    func radiusWith(_ radius: CGFloat, backgroundImage: UIImage?, contentMode: UIViewContentMode) {
         radiusWith(radius, borderColor: nil, borderWidth: 0, backgroundColor: nil, backgroundImage: backgroundImage, contentMode: contentMode)
     }
     
     /**给view设置一个 contentMode 模式的圆角背景图,你可能还需要配置边框和背景颜色*/
-    func radiusWith(radius: CGFloat, borderColor: UIColor?, borderWidth: CGFloat, backgroundColor: UIColor?, backgroundImage: UIImage?, contentMode: UIViewContentMode) {
+    func radiusWith(_ radius: CGFloat, borderColor: UIColor?, borderWidth: CGFloat, backgroundColor: UIColor?, backgroundImage: UIImage?, contentMode: UIViewContentMode) {
         jm_setRadiusWith(JMRadiusMake(radius, radius, radius, radius), borderColor: borderColor, borderWidth: borderWidth, backgroundColor: backgroundColor, backgroundImage: backgroundImage, contentMode: contentMode)
     }
     
     /**给view设置一个 .ScaleAspectFill 模式的四个圆角弧度可以不同的背景图*/
-    func JMRadiusWith(radius: JMRadius, backgroundImage: UIImage?) {
-        JMRadiusWith(radius, borderColor: nil, borderWidth: 0, backgroundColor: nil, backgroundImage: backgroundImage, contentMode: .ScaleAspectFill)
+    func JMRadiusWith(_ radius: JMRadius, backgroundImage: UIImage?) {
+        JMRadiusWith(radius, borderColor: nil, borderWidth: 0, backgroundColor: nil, backgroundImage: backgroundImage, contentMode: .scaleAspectFill)
     }
     
     /**给view设置一个 contentMode 模式的四个圆角弧度可以不同的背景图*/
-    func JMRadiusWith(radius: JMRadius, backgroundImage: UIImage?, contentMode: UIViewContentMode) {
+    func JMRadiusWith(_ radius: JMRadius, backgroundImage: UIImage?, contentMode: UIViewContentMode) {
         JMRadiusWith(radius, borderColor: nil, borderWidth: 0, backgroundColor: nil, backgroundImage: backgroundImage, contentMode: contentMode)
     }
     
     /**给view设置一个 contentMode 模式的四个圆角弧度可以不同的背景图,你可能还需要配置边框和背景颜色*/
-    func JMRadiusWith(radius: JMRadius, borderColor: UIColor?, borderWidth: CGFloat, backgroundColor: UIColor?, backgroundImage: UIImage?, contentMode: UIViewContentMode) {
+    func JMRadiusWith(_ radius: JMRadius, borderColor: UIColor?, borderWidth: CGFloat, backgroundColor: UIColor?, backgroundImage: UIImage?, contentMode: UIViewContentMode) {
         jm_setRadiusWith(radius, borderColor: borderColor, borderWidth: borderWidth, backgroundColor: backgroundColor, backgroundImage: backgroundImage, contentMode: contentMode)
     }
 }
 
 extension UIView {
     
-    private struct AssociatedKeys {
+    fileprivate struct AssociatedKeys {
         static var jm_operationKey = "jm_operation"
     }
     
-    var operation: NSOperation? {
+    var operation: Operation? {
         get {
-            return objc_getAssociatedObject(self, &AssociatedKeys.jm_operationKey) as? NSOperation
+            return objc_getAssociatedObject(self, &AssociatedKeys.jm_operationKey) as? Operation
         }
         set {
             objc_setAssociatedObject(self, &AssociatedKeys.jm_operationKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
@@ -76,36 +76,36 @@ extension UIView {
         self.operation = nil
     }
     
-    public func jm_setRadiusWith(radius: JMRadius, borderColor: UIColor?, borderWidth: CGFloat, backgroundColor: UIColor?, backgroundImage: UIImage?, contentMode: UIViewContentMode) {
+    public func jm_setRadiusWith(_ radius: JMRadius, borderColor: UIColor?, borderWidth: CGFloat, backgroundColor: UIColor?, backgroundImage: UIImage?, contentMode: UIViewContentMode) {
         self.jm_cancelOperation()
         
-        self.jm_setRadiusWith(radius, borderColor: borderColor, borderWidth: borderWidth, backgroundColor: backgroundColor, backgroundImage: backgroundImage, contentMode: contentMode, size: CGSizeZero)
+        self.jm_setRadiusWith(radius, borderColor: borderColor, borderWidth: borderWidth, backgroundColor: backgroundColor, backgroundImage: backgroundImage, contentMode: contentMode, size: CGSize.zero)
     }
     
-    public func jm_setRadiusWith(radius: JMRadius, borderColor: UIColor?, borderWidth: CGFloat, backgroundColor: UIColor?, backgroundImage: UIImage?, contentMode: UIViewContentMode, size: CGSize) {
+    public func jm_setRadiusWith(_ radius: JMRadius, borderColor: UIColor?, borderWidth: CGFloat, backgroundColor: UIColor?, backgroundImage: UIImage?, contentMode: UIViewContentMode, size: CGSize) {
         
         var _size = size
         weak var wself = self
-        let blockOperation = NSBlockOperation  {
+        let blockOperation = BlockOperation  {
             if ((wself!.operation?.cancel()) != nil) {
-                if(CGSizeEqualToSize(_size, CGSizeZero)) {
-                    dispatch_sync(dispatch_get_main_queue(), {
+                if(_size.equalTo(CGSize.zero)) {
+                    DispatchQueue.main.sync(execute: {
                         _size = wself!.bounds.size
                     })
                 }
             }
-            _size = CGSizeMake(pixel(_size.width), pixel(_size.height))
+            _size = CGSize(width: pixel(_size.width), height: pixel(_size.height))
             let image = UIImage.jm_setRadiusWith(_size, radius: radius, borderColor: borderColor, borderWidth: borderWidth, backgroundColor: backgroundColor, backgroundImage: backgroundImage, contentMode: contentMode)
-            NSOperationQueue.mainQueue().addOperationWithBlock {
-                wself!.frame = CGRectMake(pixel(wself!.frame.origin.x), pixel(wself!.frame.origin.y), _size.width, _size.height)
+            OperationQueue.main.addOperation {
+                wself!.frame = CGRect(x: pixel(wself!.frame.origin.x), y: pixel(wself!.frame.origin.y), width: _size.width, height: _size.height)
                 if wself is UIImageView {
                     (wself as! UIImageView).image = image
                 } else if wself is UIButton && backgroundImage != nil {
-                    (wself as! UIButton).setBackgroundImage(image, forState: .Normal)
+                    (wself as! UIButton).setBackgroundImage(image, for: UIControlState())
                 } else if wself is UILabel {
-                    wself!.layer.backgroundColor = UIColor.init(patternImage: image).CGColor
+                    wself!.layer.backgroundColor = UIColor.init(patternImage: image).cgColor
                 } else {
-                    wself!.layer.contents = image.CGImage
+                    wself!.layer.contents = image.cgImage
                 }
             }
         }
@@ -114,30 +114,30 @@ extension UIView {
     }
 }
 
-private func pixel(num: CGFloat) -> CGFloat {
+private func pixel(_ num: CGFloat) -> CGFloat {
     
-    let unit = 1.0 / UIScreen.mainScreen().scale
+    let unit = 1.0 / UIScreen.main.scale
     let remain = CGFloat(fmodf(Float(num), Float(unit)))
     return num - remain + (remain >= unit / 2.0 ? unit: 0)
 }
 
 extension UIImage {
     
-    static func jm_setRadiusWith(size: CGSize, radius: JMRadius, borderColor: UIColor?, borderWidth: CGFloat, backgroundColor: UIColor?, backgroundImage: UIImage?, contentMode: UIViewContentMode) -> UIImage {
+    static func jm_setRadiusWith(_ size: CGSize, radius: JMRadius, borderColor: UIColor?, borderWidth: CGFloat, backgroundColor: UIColor?, backgroundImage: UIImage?, contentMode: UIViewContentMode) -> UIImage {
         var backgroundColor = backgroundColor
         if var image = backgroundImage {
             image = image.scaleWith(size, backgroundColor: backgroundColor, contentMode: contentMode)
             backgroundColor = UIColor.init(patternImage: image)
         } else if backgroundColor == nil {
-            backgroundColor = UIColor.whiteColor()
+            backgroundColor = UIColor.white
         }
         
-        UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.mainScreen().scale)
+        UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.main.scale)
         let halfBorderWidth = borderWidth / 2
         let context = UIGraphicsGetCurrentContext()
-        CGContextSetLineWidth(context, borderWidth)
-        CGContextSetStrokeColorWithColor(context, borderColor?.CGColor)
-        CGContextSetFillColorWithColor(context, backgroundColor?.CGColor)
+        context?.setLineWidth(borderWidth)
+        context?.setStrokeColor((borderColor?.cgColor)!)
+        context?.setFillColor((backgroundColor?.cgColor)!)
         let height = size.height
         let width = size.width
         let radius = transformationJMRadius(radius, size: size, borderWidth: borderWidth)
@@ -148,49 +148,49 @@ extension UIImage {
             startPointY = halfBorderWidth + radius.topRightRadius
         }
         
-        CGContextMoveToPoint(context, width - halfBorderWidth, startPointY)// 开始坐标右边开始
+        context?.move(to: CGPoint(x: width - halfBorderWidth, y: startPointY))// 开始坐标右边开始
         CGContextAddArcToPoint(context, width - halfBorderWidth, height - halfBorderWidth, width / 2, height - halfBorderWidth, radius.bottomRightRadius)// 右下角角度
         CGContextAddArcToPoint(context, halfBorderWidth, height - halfBorderWidth, halfBorderWidth, height / 2, radius.bottomLeftRadius)// 左下角角度
         CGContextAddArcToPoint(context, halfBorderWidth, halfBorderWidth, width / 2, halfBorderWidth, radius.topLeftRadius)// 左上角
         CGContextAddArcToPoint(context, width - halfBorderWidth, halfBorderWidth, width - halfBorderWidth, height / 2, radius.topRightRadius)// 右上角
-        CGContextDrawPath(context, .FillStroke) //根据坐标绘制路径
+        context?.drawPath(using: .fillStroke) //根据坐标绘制路径
         
         let outImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        return outImage
+        return outImage!
     }
     
-    private func scaleWith(size: CGSize, backgroundColor: UIColor?, contentMode: UIViewContentMode) -> UIImage {
+    fileprivate func scaleWith(_ size: CGSize, backgroundColor: UIColor?, contentMode: UIViewContentMode) -> UIImage {
         
-        let rect = CGRectMake(0, 0, size.width, size.height)
-        UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.mainScreen().scale)
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.main.scale)
         if let color = backgroundColor {
             let context = UIGraphicsGetCurrentContext()
-            CGContextSetFillColorWithColor(context, color.CGColor)
-            CGContextAddRect(context, rect)
-            CGContextDrawPath(context, .FillStroke)
+            context?.setFillColor(color.cgColor)
+            context?.addRect(rect)
+            context?.drawPath(using: .fillStroke)
         }
-        drawInRect(convertRect(CGRectMake(0.0, 0.0, size.width, size.height), contentMode: contentMode))
+        draw(in: convertRect(CGRect(x: 0.0, y: 0.0, width: size.width, height: size.height), contentMode: contentMode))
         let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        return scaledImage
+        return scaledImage!
     }
     
-    private func convertRect(rect: CGRect, contentMode: UIViewContentMode) -> CGRect {
+    fileprivate func convertRect(_ rect: CGRect, contentMode: UIViewContentMode) -> CGRect {
         
         var size = self.size
-        var rect = CGRectStandardize(rect)
+        var rect = rect.standardized
         size.width = size.width < 0 ? -size.width : size.width
         size.height = size.height < 0 ? -size.height : size.height
-        let center = CGPointMake(CGRectGetMidX(rect), CGRectGetMidY(rect))
+        let center = CGPoint(x: rect.midX, y: rect.midY)
         switch contentMode {
-        case .Redraw, .ScaleAspectFit, .ScaleAspectFill:
+        case .redraw, .scaleAspectFit, .scaleAspectFill:
             if rect.size.width < 0.01 || rect.size.height < 0.01 || size.width < 0.01 || size.height < 0.01 {
                 rect.origin = center
-                rect.size = CGSizeZero
+                rect.size = CGSize.zero
             } else {
                 var scale: CGFloat
-                if contentMode == .ScaleAspectFill {
+                if contentMode == .scaleAspectFill {
                     if size.width / size.height < rect.size.width / rect.size.height {
                         scale = rect.size.width / size.width
                     } else {
@@ -206,34 +206,34 @@ extension UIImage {
                 size.width *= scale
                 size.height *= scale
                 rect.size = size
-                rect.origin = CGPointMake(center.x - size.width * 0.5, center.y - size.height * 0.5)
+                rect.origin = CGPoint(x: center.x - size.width * 0.5, y: center.y - size.height * 0.5)
             }
-        case .Center:
+        case .center:
             rect.size = size
-            rect.origin = CGPointMake(center.x - size.width * 0.5, center.y - size.height * 0.5)
-        case .Top:
+            rect.origin = CGPoint(x: center.x - size.width * 0.5, y: center.y - size.height * 0.5)
+        case .top:
             rect.origin.x = center.x - size.width * 0.5
             rect.size = size
-        case .Bottom:
+        case .bottom:
             rect.origin.x = center.x - size.width * 0.5
             rect.origin.y += rect.size.height - size.height
             rect.size = size
-        case .Left:
+        case .left:
             rect.origin.y = center.y - size.height * 0.5
             rect.size = size
-        case .Right:
+        case .right:
             rect.origin.y = center.y - size.height * 0.5
             rect.origin.x += rect.size.width - size.width
             rect.size = size
-        case .TopLeft:
+        case .topLeft:
             rect.size = size
-        case .TopRight:
+        case .topRight:
             rect.origin.x += rect.size.width - size.width
             rect.size = size
-        case .BottomLeft:
+        case .bottomLeft:
             rect.origin.y += rect.size.height - size.height
             rect.size = size
-        case .BottomRight:
+        case .bottomRight:
             rect.origin.x += rect.size.width - size.width
             rect.origin.y += rect.size.height - size.height
             rect.size = size
@@ -244,7 +244,7 @@ extension UIImage {
 }
 
 //根据半径优先级算出新的JMRadius
-private func transformationJMRadius(radius: JMRadius, size: CGSize, borderWidth: CGFloat) -> JMRadius{
+private func transformationJMRadius(_ radius: JMRadius, size: CGSize, borderWidth: CGFloat) -> JMRadius{
     
     var radius = radius
     radius.topLeftRadius = minimum(size.width - borderWidth, size.height - borderWidth, radius.topLeftRadius - borderWidth / 2)
@@ -254,7 +254,7 @@ private func transformationJMRadius(radius: JMRadius, size: CGSize, borderWidth:
     return radius
 }
 
-private func minimum(a: CGFloat, _ b: CGFloat, _ c: CGFloat) -> CGFloat {
+private func minimum(_ a: CGFloat, _ b: CGFloat, _ c: CGFloat) -> CGFloat {
     
     return max(min(min(a, b), c), 0)
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## 此库太久没更新了，请勿使用
+
 # JMRoundedCornerSwift
 
 UIView settings without triggering off-screen rendering fillet /UIView设置不触发离屏渲染的圆角 
